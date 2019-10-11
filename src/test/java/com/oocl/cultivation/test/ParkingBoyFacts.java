e30eef06830a4306301951bf9e150d09ad5f2ea2
chulowee <katrina.chloe.reyes@oocl.com>
2019-10-11 15:21:10 +0800

    Story1 AC1 - AC:3

--- src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
@@ -5,12 +5,61 @@ import com.oocl.cultivation.ParkingBoy;
 import com.oocl.cultivation.ParkingLot;
 import com.oocl.cultivation.ParkingTicket;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParkingBoyFacts {
     @Test
-    void write_your_first_test() {
+    void should_park_car_in_parking_lot_by_parking_boy(){
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
 
+        assertNotNull(parkingLot.park(car));
     }
+
+    @Test
+    void should_fetch_car_by_parking_ticket_from_parking_lot() {
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+
+        ParkingTicket park = parkingBoy.park(car);
+
+        assertEquals(parkingBoy.fetch(park), car);
+    }
+
+    @Test
+    void should_parking_boy_park_multiple_cars() {
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car myCar = new Car();
+        Car yourCar = new Car();
+
+//        when
+        ParkingTicket ticket = parkingBoy.park(myCar);
+        parkingBoy.park(yourCar);
+
+        Car fetchedCar = parkingBoy.fetch(ticket);
+
+        assertSame(myCar, fetchedCar);
+        assertNotSame(yourCar, fetchedCar);
+    }
+
+    @Test
+    void should_parking_boy_return_no_cars_with_wrong_ticket() {
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car myCar = new Car();
+        Car yourCar = new Car();
+
+        parkingBoy.park(yourCar);
+
+        Car fetchedCar = parkingBoy.fetch(new ParkingTicket());
+
+        assertNotSame(myCar, fetchedCar);
+    }
+
 }
